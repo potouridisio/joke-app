3a3ffd5f42789e514ca2b668238e213ef8ab4bf6
PanosG <shiro.miyume@outlook.com>
2024-03-26 23:07:11 +0200

    Retrieve selected categories

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,28 @@ import { fetchJoke } from './utils'
 // https://www.typescripttutorial.net/typescript-tutorial/type-casting/
 const jokeElement = document.getElementById('joke') as HTMLElement;
 const categoryForm = document.getElementById('categoryForm') as HTMLElement;
+const progCategory = document.getElementsByName('category')[0] as HTMLInputElement;
+const miscCategory = document.getElementsByName('category')[1] as HTMLInputElement;
+const darkCategory = document.getElementsByName('category')[2] as HTMLInputElement;
+const punCategory = document.getElementsByName('category')[3] as HTMLInputElement;
+const spookyCategory = document.getElementsByName('category')[4] as HTMLInputElement;
+const christCategory = document.getElementsByName('category')[5] as HTMLInputElement;
 
+const categories = [progCategory, miscCategory, darkCategory, punCategory, spookyCategory, christCategory];
 categoryForm.addEventListener('submit', async (event) => {
   event.preventDefault(); // Prevent default form submission behavior
 
   // Retrieve selected categories
   const selectedCategories: Category[] = [];
+
+  categories.forEach((category) => {
+    if (category.checked) {
+
+      if (category.value === 'Programming' || category.value === 'Misc' || category.value === 'Dark' || category.value === 'Pun' || category.value === 'Spooky' || category.value === 'Christmas') {
+        selectedCategories.push(category.value);   
+      }
+    }
+  });
 
   const joke = await fetchJoke(selectedCategories);
 
